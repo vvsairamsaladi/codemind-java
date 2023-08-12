4112f5e26f273fb07836871d0219023e6b430f92
codemind <119723659+vvsairamsaladi@users.noreply.github.com>
2023-08-12 11:42:47 +0530

    import java.util.Scanner;      public class sr     {         public static void main(String[] args)         {             int a,b,c;             Scanner sc =  new Scanner(System.in);             a = sc.nextInt();             b = sc.nextInt();             c = (a*b);             System.out.printf("%d",c);         }     }

---
+++ Multiplication.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+     public class sr
+    {
+        public static void main(String[] args)
+        {
+            int a,b,c;
+            Scanner sc =  new Scanner(System.in);
+            a = sc.nextInt();
+            b = sc.nextInt();
+            c = (a*b);
+            System.out.printf("%d",c);
+        }
+    }
