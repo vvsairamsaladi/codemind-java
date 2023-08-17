3388c3fcdf13aad5b716b5690f821d20798f2af0
codemind <119723659+vvsairamsaladi@users.noreply.github.com>
2023-08-17 12:09:54 +0530

    import java.util.Scanner; public class Sa {     public static void main(String[] args)     {         Scanner sc = new Scanner(System.in);         String a = sc.nextLine();         System.out.print(a);
        }
    }

---
+++ Take_Input_And_Print.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Sa
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.nextLine();
+        System.out.print(a);
+        
+    }
+}
