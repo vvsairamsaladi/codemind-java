89bacb788a9cb59ef16c297a9f9a0767d9d18164
codemind <119723659+vvsairamsaladi@users.noreply.github.com>
2023-08-17 11:45:12 +0530

    import java.util.Scanner; public class Sa {     public static void main(String[] args)     {         Scanner sc = new Scanner(System.in);         int a = sc.nextInt();         if(a==2)         {             System.out.println("1");         }         else if(a==1)         {             System.out.println("0");         }         else         {             System.out.println("2");         }     } }

---
+++ The_Pandemic.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Sa
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        if(a==2)
+        {
+            System.out.println("1");
+        }
+        else if(a==1)
+        {
+            System.out.println("0");
+        }
+        else
+        {
+            System.out.println("2");
+        }
+    }
+}
