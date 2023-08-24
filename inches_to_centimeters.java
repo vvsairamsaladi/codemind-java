594e4aa976d202e53495c6ee40d7932722b811f1
codemind <119723659+vvsairamsaladi@users.noreply.github.com>
2023-08-24 11:29:37 +0530

    import java.util.Scanner; public class In {     public static void main(String[] args)     {         Scanner sc = new Scanner(System.in);         double i = sc.nextDouble();         System.out.printf("%.2f",2.54*i);     }
    }

---
+++ inches_to_centimeters.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class In
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        double i = sc.nextDouble();
+        System.out.printf("%.2f",2.54*i);
+    }
+    
+}
